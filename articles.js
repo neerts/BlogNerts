8962672bd56c9211990500d8994d0ce9a7cba423
neerts <nerts53@gmail.com>
2025-09-09 19:31:05 +0700

    Add files via upload

---
+++ articles.js
@@ -0,0 +1,16 @@
+const defaultArticles = [
+  {
+    title: "s A",
+    description: "Deskripsi untuk menjelaskan atau menaruh link dengan nama custom",
+    content: "Konten inti artikelmu di sini. Bisa beberapa paragraf.",
+    youtubeUrl: "https://youtu.be/KQeQCkA5i10?si=0FWY9vP9msjVvqcc",
+    categories: ["Legend"]
+  },
+  {
+    title: "s B",
+    description: "Buka artikel secara acak dari daftar.",
+    content: "Gunakan Math.random dan pindah ke halaman artikel.",
+    youtubeUrl: "https://youtu.be/ovjJSI0ce0c?si=5HRQqqJykri3qaoQ",
+    categories: ["Epic"]
+  }
+];
