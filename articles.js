5d93518f4c2d34edb34446690f0a1aabd0e23bb7
neerts <nerts53@gmail.com>
2025-09-09 19:48:42 +0700

    Update articles.js

--- articles.js
+++ articles.js
@@ -3,14 +3,14 @@ const defaultArticles = [
     title: "s A",
     description: "Deskripsi untuk menjelaskan atau menaruh link dengan nama custom",
     content: "Konten inti artikelmu di sini. Bisa beberapa paragraf.",
-    youtubeUrl: "https://youtu.be/KQeQCkA5i10?si=0FWY9vP9msjVvqcc",
+    youtubeUrl: "https://youtu.be/hB7CDrVnNCs?si=pzdFT80cKwH9XTDy",
     categories: ["Legend"]
   },
   {
     title: "s B",
     description: "Buka artikel secara acak dari daftar.",
     content: "Gunakan Math.random dan pindah ke halaman artikel.",
-    youtubeUrl: "https://youtu.be/ovjJSI0ce0c?si=5HRQqqJykri3qaoQ",
+    youtubeUrl: "https://youtu.be/hB7CDrVnNCs?si=pzdFT80cKwH9XTDy",
     categories: ["Epic"]
   }
 ];
