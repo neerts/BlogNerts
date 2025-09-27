bd099a4b81095eb69f5d4b4b3e6d1bc03f8a20f3
neerts <nerts53@gmail.com>
2025-09-27 19:34:04 +0700

    Update articles.js

--- articles.js
+++ articles.js
@@ -1,10 +1,50 @@
 const defaultArticles = [
   {
-    title: "s A",
-    description: "Deskripsi untuk menjelaskan atau menaruh link dengan nama custom",
-    content: "Konten inti artikelmu di sini. Bisa beberapa paragraf.",
-    youtubeUrl: "https://youtu.be/hB7CDrVnNCs?si=pzdFT80cKwH9XTDy",
-    categories: ["Legend"]
+    title: "SHARE SCRIPT SKIN ANGELA SANRIO - HEARTSTRING | NO PASSWORD | FULL EFFECT | PATCH TERBARU",
+    description: "▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ 
+
+➙ Cari Skin Lainnya : 
+Coming Soon!
+➙ Discord Community :
+Coming Soon!
+➙ Support Nerts,
+✤ Saweria : 
+https://saweria.co/nerts
+➙ Subscribe :
+https://youtube.com/@Nertsofficial2?si=pe8E5WFSZ8LOKmlf
+
+◈ Like video ini ✓
+◈ Subscribe channel ini ✓
+◈ Coment positif ✓
+◈ Share ke temen kalian✓
+▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ 
+
+📌 Fitur :
+• Full Effect & Voice
+• No Banned
+• No Root
+• Work All Mode
+• Work 100%
+▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ 
+
+",
+    content: "📌 DOWNLOAD HERE : 
+
+Script Angela Sanrio No Password⬇️
+
+Replace: Default
+https://sfl.gl/aETmuFT7
+
+Replace: Basic
+https://sfl.gl/36e0yk
+
+Replace: Starlight
+https://sfl.gl/ZtEYYpaT
+
+Replace: Summer
+https://sfl.gl/VC632a",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Sanrio"]
   },
   {
     title: "s B",
