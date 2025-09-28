5bca6db6613becae233ada9edaa06cf6dafa99f6
neerts <nerts53@gmail.com>
2025-09-28 12:57:46 +0700

    update articles.js

--- articles.js
+++ articles.js
@@ -1,5 +1,40 @@
 const defaultArticles = [
   {
+    title: "SCRIPT SKIN ANGELA KINSHIN - STRINGS OF FATE",
+    description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/a9ssI7p54IE\nReplace Basic :\nhttps://sfile.mobi/5vmq9qaKZOp\nReplace Summer :\nhttps://sfile.mobi/37CoQd34zS3\nReplace Halloween :\nhttps://sfile.mobi/aD0sZjn6RUK\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Kinshin"]
+  },
+  {
+    title: "SCRIPT SKIN KARRIE KINSHIN - Breath Of Naraka",
+    description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/94Ys8tgAusZ\nReplace Basic :\nhttps://sfile.mobi/5vmqaiKxUWz\nReplace Elite :\nhttps://sfile.mobi/a24sFCNlbqP\nReplace Halloween :\nhttps://sfile.mobi/6dGqziAb9St\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Kinshin"]
+  },
+  {
+    title: "SCRIPT SKIN LANCELOT KINSHIN - Guardian Of The Shine",
+    description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/51OpUH1kX6E\nReplace Basic :\nhttps://sfile.mobi/aRMt8Htx9Ci\nReplace Elite :\nhttps://sfile.mobi/4N2pMT9Jtur\nReplace Epic :\nhttps://sfile.mobi/4UqpS1uQ52F\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Kinshin"]
+  },
+  {
+    title: "SCRIPT SKIN BEATRIX FMVP - Sky Force Maverick",
+    description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/bA6trIbp4W7\nReplace Elite :\nhttps://sfile.mobi/9r8sgEKQ5a3\nReplace Special :\nhttps://sfile.mobi/3mooVJ0PiEr\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Fmvp"]
+  },
+  {
+    title: "SCRIPT SKIN JOY CHAMPION - ONIC PH",
+    description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/94Ys14OLhCq\nReplace Basic :\nhttps://sfile.mobi/59cpQQFNwkq\nReplace Elite :\nhttps://sfile.mobi/4FEpAT74BGl\nReplace Starlight :\nhttps://sfile.mobi/aKosYsAs6s3\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    youtubeUrl: "https://youtu.be/aHQf-m55wlU",
+    categories: ["Champion"]
+  },
+  {
     title: "SCRIPT SKIN ANGELA SANRIO - HEARTSTRING",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
     content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/7T6q38jPFYV\nReplace Basic :\nhttps://sfile.mobi/2SQnicxPqE0\nReplace Summer :\nhttps://sfile.mobi/59coy2tZ3am\nReplace Starlight :\nhttps://sfile.mobi/6dGp8V1Mv6X\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
