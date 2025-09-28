696b87bcacabe1cefe828d0c2afc578674b601b1
neerts <nerts53@gmail.com>
2025-09-28 11:38:32 +0700

    update articles.js

--- articles.js
+++ articles.js
@@ -2,28 +2,28 @@ const defaultArticles = [
   {
     title: "SCRIPT SKIN ANGELA SANRIO - HEARTSTRING",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/7T6q38jPFYV\nReplace Basic :\nhttps://sfile.mobi/2SQnicxPqE0\nReplace Summer :\nhttps://sfile.mobi/59coy2tZ3am\nReplace Starlight :\nhttps://sfile.mobi/6dGp8V1Mv6X\n\n⚠️ How To Download?\nLink\nUpdate Artikel 27/9/25",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/7T6q38jPFYV\nReplace Basic :\nhttps://sfile.mobi/2SQnicxPqE0\nReplace Summer :\nhttps://sfile.mobi/59coy2tZ3am\nReplace Starlight :\nhttps://sfile.mobi/6dGp8V1Mv6X\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
     youtubeUrl: "https://youtu.be/aHQf-m55wlU",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN FLORYN SANRIO - FLUFFY DREAM",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default/Basic :\nhttps://sfile.mobi/aRMsLsDvEcS\nReplace Elite :\nhttps://sfile.mobi/aD0sDtR9cWe\n\n⚠️ How To Download?\nLink\nUpdate Artikel 27/9/25",
+    content: "🗂️ Download Script :\nReplace Default/Basic :\nhttps://sfile.mobi/aRMsLsDvEcS\nReplace Elite :\nhttps://sfile.mobi/aD0sDtR9cWe\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
     youtubeUrl: "https://youtu.be/Gc7fi0NUrxM",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN CHANGE : SANRIO - MOON ARTIST",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/8IOrAL2sDeJ\nReplace Basic :\nhttps://sfile.mobi/5vmpOP46cwq\nReplace Elite :\nhttps://sfile.mobi/6OCqy2dM4wV\nReplace Special :\nhttps://sfile.mobi/1VKnR0nzzOd\n\n⚠️ How To Download?\nLink\nUpdate Artikel 27/9/25",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/8IOrAL2sDeJ\nReplace Basic :\nhttps://sfile.mobi/5vmpOP46cwq\nReplace Elite :\nhttps://sfile.mobi/6OCqy2dM4wV\nReplace Special :\nhttps://sfile.mobi/1VKnR0nzzOd\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
     youtubeUrl: "https://youtu.be/as4jlvEpC1w",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN CLAUDE SANRIO - BAD BRO",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/1zAnEWKEoUc\nReplace Basic :\nhttps://sfile.mobi/3XkoYGCBLiL\nReplace Summer :\nhttps://sfile.mobi/3IyoQHQfiUi\nReplace Starlight :\nhttps://sfile.mobi/agQstbnNPqB\n\n⚠️ How To Download?\nLink\nUpdate Artikel 27/9/25",
+    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfile.mobi/1zAnEWKEoUc\nReplace Basic :\nhttps://sfile.mobi/3XkoYGCBLiL\nReplace Summer :\nhttps://sfile.mobi/3IyoQHQfiUi\nReplace Starlight :\nhttps://sfile.mobi/agQstbnNPqB\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
     youtubeUrl: "https://youtu.be/eUiyXPr2L-o",
     categories: ["Sanrio"]
   }
