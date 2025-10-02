1fd95c8f8197af776d1f1bc5a9138093b4a3a849
neerts <nerts53@gmail.com>
2025-10-02 21:38:59 +0700

    update articles.js

--- articles.js
+++ articles.js
@@ -2,119 +2,359 @@ const defaultArticles = [
   {
     title: "SCRIPT SKIN BRODY NEOBEASTS - NEOBEASTS BRODY",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/HgA9\nReplace Default Logo :\nhttps://sfl.gl/damdaH\nReplace Basic :\nhttps://sfl.gl/83yhPDd\nReplace Basic Logo :\nhttps://sfl.gl/2U8NFoQd\nReplace Starlight :\nhttps://sfl.gl/r6DQE\nReplace Mpl :\nhttps://sfl.gl/I3CN\nReplace Champion :\nhttps://sfl.gl/P8JRuKhT\nReplace Stun :\nhttps://sfl.gl/A6RR2\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/HgA9
+  Replace Default Logo :
+  https://sfl.gl/damdaH
+  Replace Basic :
+  https://sfl.gl/83yhPDd
+  Replace Basic Logo :
+  https://sfl.gl/2U8NFoQd
+  Replace Starlight :
+  https://sfl.gl/r6DQE
+  Replace Mpl :
+  https://sfl.gl/I3CN
+  Replace Champion :
+  https://sfl.gl/P8JRuKhT
+  Replace Stun :
+  https://sfl.gl/A6RR2
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/uqTXAopgu_4",
     categories: ["Neobeasts"]
   },
   {
     title: "SCRIPT SKIN PHARSA NEOBEASTS - NEOBEASTS PHARSA",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/Pp0Lw\nReplace Default Logo :\nhttps://sfl.gl/AHlxIYR\nReplace Basic :\nhttps://sfl.gl/SQVF8\nReplace Basic Logo :\nhttps://sfl.gl/FiQR7\nReplace Elite :\nhttps://sfl.gl/wJTIbzEB\nReplace Season :\nhttps://sfl.gl/YEXDeiq\nReplace Special :\nhttps://sfl.gl/xYTXM\nReplace Starlight :\nhttps://sfl.gl/PiQtc\nReplace Epic :\nhttps://sfl.gl/p6pLOb\nReplace Indigo Aviatrix :\nhttps://sfl.gl/ioAfuI\nReplace Collector :\nhttps://sfl.gl/d1E7n5\nReplace Msc :\nhttps://sfl.gl/8FcGRx\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/Pp0Lw
+  Replace Default Logo :
+  https://sfl.gl/AHlxIYR
+  Replace Basic :
+  https://sfl.gl/SQVF8
+  Replace Basic Logo :
+  https://sfl.gl/FiQR7
+  Replace Elite :
+  https://sfl.gl/wJTIbzEB
+  Replace Season :
+  https://sfl.gl/YEXDeiq
+  Replace Special :
+  https://sfl.gl/xYTXM
+  Replace Starlight :
+  https://sfl.gl/PiQtc
+  Replace Epic :
+  https://sfl.gl/p6pLOb
+  Replace Indigo Aviatrix :
+  https://sfl.gl/ioAfuI
+  Replace Collector :
+  https://sfl.gl/d1E7n5
+  Replace Msc :
+  https://sfl.gl/8FcGRx
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/7wiSYCL_x_I",
     categories: ["Neobeasts"]
   },
   {
     title: "SCRIPT SKIN LING NEOBEASTS - NEOBEASTS LING",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/6pSYK\nReplace Default Logo :\nhttps://sfl.gl/XjQvaxiF\nReplace Basic :\nhttps://sfl.gl/NfASg\nReplace Basic Logo :\nhttps://sfl.gl/ai6I9M\nReplace Special :\nhttps://sfl.gl/5SwB79\nReplace Starlight :\nhttps://sfl.gl/LYoBXqz0\nReplace M Word :\nhttps://sfl.gl/1vBh6S\nReplace Dragon Tamer :\nhttps://sfl.gl/NSBAWgz\nReplace Collector :\nhttps://sfl.gl/CzAk\nReplace Kung Fu Panda :\nhttps://sfl.gl/NBD1AT\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/6pSYK
+  Replace Default Logo :
+  https://sfl.gl/XjQvaxiF
+  Replace Basic :
+  https://sfl.gl/NfASg
+  Replace Basic Logo :
+  https://sfl.gl/ai6I9M
+  Replace Special :
+  https://sfl.gl/5SwB79
+  Replace Starlight :
+  https://sfl.gl/LYoBXqz0
+  Replace M Word :
+  https://sfl.gl/1vBh6S
+  Replace Dragon Tamer :
+  https://sfl.gl/NSBAWgz
+  Replace Collector :
+  https://sfl.gl/CzAk
+  Replace Kung Fu Panda :
+  https://sfl.gl/NBD1AT
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/ji0-ObchVps",
     categories: ["Neobeasts"]
   },
   {
     title: "SCRIPT SKIN LYLIA NEOBEASTS - NEOBEASTS LYLIA",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/30OXFx4\nReplace Default Logo :\nhttps://sfl.gl/gjiS3mW\nReplace Basic :\nhttps://sfl.gl/Nek1R8ba\nReplace Basic Logo :\nhttps://sfl.gl/Y3P9z\nReplace Elite :\nhttps://sfl.gl/c3hgDQqG\nReplace Halloween :\nhttps://sfl.gl/ICeuj\nReplace Special :\nhttps://sfl.gl/1gfYSfl\nReplace Starlight :\nhttps://sfl.gl/xQHVXn\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/30OXFx4
+  Replace Default Logo :
+  https://sfl.gl/gjiS3mW
+  Replace Basic :
+  https://sfl.gl/Nek1R8ba
+  Replace Basic Logo :
+  https://sfl.gl/Y3P9z
+  Replace Elite :
+  https://sfl.gl/c3hgDQqG
+  Replace Halloween :
+  https://sfl.gl/ICeuj
+  Replace Special :
+  https://sfl.gl/1gfYSfl
+  Replace Starlight :
+  https://sfl.gl/xQHVXn
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/BzzD5OIM-M4?si=lMhvF5jApYrA9QS8",
     categories: ["Neobeasts"]
   },
   {
     title: "SCRIPT SKIN FREDRINN NEOBEASTS - NEOBEASTS FREDRINN",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/usgLuMy6\nReplace Basic :\nhttps://sfl.gl/5Xypyiy\nReplace Elite :\nhttps://sfl.gl/eplYD\nReplace Sparkle :\nhttps://sfl.gl/IU1xuSc\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/usgLuMy6
+  Replace Basic :
+  https://sfl.gl/5Xypyiy
+  Replace Elite :
+  https://sfl.gl/eplYD
+  Replace Sparkle :
+  https://sfl.gl/IU1xuSc
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/_DSDagl0G1Y",
     categories: ["Neobeasts"]
   },
   {
     title: "SCRIPT SKIN IXIA METRO ZERO - INVOKER'S COMMAND",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/OnorDhY8\nReplace Basic :\nhttps://sfl.gl/gxkr2\nReplace Starlight :\nhttps://sfl.gl/dmaAxWY\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/OnorDhY8
+  Replace Basic :
+  https://sfl.gl/gxkr2
+  Replace Starlight :
+  https://sfl.gl/dmaAxWY
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/BzzD5OIM-M4",
     categories: ["Metro Zero"]
   },
   {
     title: "SCRIPT SKIN ROGER METRO ZERO - INVOKER'S RESTRAINTS",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/kiK7LH3W\nReplace Basic :\nhttps://sfl.gl/uUpk\nReplace Epic Phantom :\nhttps://sfl.gl/gFeH\nReplace Epic Cyborg :\nhttps://sfl.gl/Qfpinp\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/kiK7LH3W
+  Replace Basic :
+  https://sfl.gl/uUpk
+  Replace Epic Phantom :
+  https://sfl.gl/gFeH
+  Replace Epic Cyborg :
+  https://sfl.gl/Qfpinp
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/hrt_oB1pN7E",
     categories: ["Metro Zero"]
   },
   {
     title: "SCRIPT SKIN X'BORG METRO ZERO - INVOKER'S FLAME",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/jBRb1\nReplace Basic :\nhttps://sfl.gl/pO3sNK8\nReplace Elite :\nhttps://sfl.gl/3GkhsL5\nReplace Starlight :\nhttps://sfl.gl/xppPmPd\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/jBRb1
+  Replace Basic :
+  https://sfl.gl/pO3sNK8
+  Replace Elite :
+  https://sfl.gl/3GkhsL5
+  Replace Starlight :
+  https://sfl.gl/xppPmPd
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/wUAgNIsTPTg",
     categories: ["Metro Zero"]
   },
   {
     title: "SCRIPT SKIN ANGELA KINSHIN - STRINGS OF FATE",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/vjLu\nReplace Basic :\nhttps://sfl.gl/ZVOjqH\nReplace Summer :\nhttps://sfl.gl/pamKot\nReplace Halloween :\nhttps://sfl.gl/UJHRNTs\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/vjLu
+  Replace Basic :
+  https://sfl.gl/ZVOjqH
+  Replace Summer :
+  https://sfl.gl/pamKot
+  Replace Halloween :
+  https://sfl.gl/UJHRNTs
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/9hnSfw_Gkas",
     categories: ["Kinshin"]
   },
   {
     title: "SCRIPT SKIN KARRIE KINSHIN - Breath Of Naraka",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/VfMIHU\nReplace Basic :\nhttps://sfl.gl/K6pmc\nReplace Elite :\nhttps://sfl.gl/rfScQ6I\nReplace Halloween :\nhttps://sfl.gl/H5dSMY6\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/VfMIHU
+  Replace Basic :
+  https://sfl.gl/K6pmc
+  Replace Elite :
+  https://sfl.gl/rfScQ6I
+  Replace Halloween :
+  https://sfl.gl/H5dSMY6
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/k6gP2a-8QLI",
     categories: ["Kinshin"]
   },
   {
     title: "SCRIPT SKIN LANCELOT KINSHIN - Guardian Of The Shine",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/TdUnFiD\nReplace Basic :\nhttps://sfl.gl/JeyU2tn\nReplace Elite :\nhttps://sfl.gl/sZEY\nReplace Epic :\nhttps://sfl.gl/arI8DFEB\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/TdUnFiD
+  Replace Basic :
+  https://sfl.gl/JeyU2tn
+  Replace Elite :
+  https://sfl.gl/sZEY
+  Replace Epic :
+  https://sfl.gl/arI8DFEB
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/Oaonk9K2HbA",
     categories: ["Kinshin"]
   },
   {
     title: "SCRIPT SKIN BEATRIX FMVP - Sky Force Maverick",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default/Basic :\nhttps://sfl.gl/Yw6or7Wh\nReplace Elite :\nhttps://sfl.gl/1fGPd\nReplace Special :\nhttps://sfl.gl/MSEy20mQ\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default/Basic :
+  https://sfl.gl/Yw6or7Wh
+  Replace Elite :
+  https://sfl.gl/1fGPd
+  Replace Special :
+  https://sfl.gl/MSEy20mQ
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/FCJuOA1nKz4",
     categories: ["Fmvp"]
   },
   {
     title: "SCRIPT SKIN JOY CHAMPION - ONIC PH",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/xY3Owu\nReplace Basic :\nhttps://sfl.gl/i4eH\nReplace Elite :\nhttps://sfl.gl/QJLuJCFl\nReplace Starlight :\nhttps://sfl.gl/oW16agbk\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/xY3Owu
+  Replace Basic :
+  https://sfl.gl/i4eH
+  Replace Elite :
+  https://sfl.gl/QJLuJCFl
+  Replace Starlight :
+  https://sfl.gl/oW16agbk
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/0jKheOEBdrk",
     categories: ["Champion"]
   },
   {
     title: "SCRIPT SKIN ANGELA SANRIO - HEARTSTRING",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/aETmuFT7\nReplace Basic :\nhttps://sfl.gl/36e0yk\nReplace Summer :\nhttps://sfl.gl/VC632a\nReplace Starlight :\nhttps://sfl.gl/ZtEYYpaT\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/aETmuFT7
+  Replace Basic :
+  https://sfl.gl/36e0yk
+  Replace Summer :
+  https://sfl.gl/VC632a
+  Replace Starlight :
+  https://sfl.gl/ZtEYYpaT
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/aHQf-m55wlU",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN FLORYN SANRIO - FLUFFY DREAM",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default/Basic :\nhttps://sfl.gl/zVhfG7U\nReplace Elite :\nhttps://sfl.gl/M8kW7MK\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default/Basic :
+  https://sfl.gl/zVhfG7U
+  Replace Elite :
+  https://sfl.gl/M8kW7MK
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/Gc7fi0NUrxM",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN CHANGE SANRIO - MOON ARTIST",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/Bkc4F\nReplace Basic :\nhttps://sfl.gl/ZVPq\nReplace Elite :\nhttps://sfl.gl/7iTbDytT\nReplace Special :\nhttps://sfl.gl/yMuAf\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/Bkc4F
+  Replace Basic :
+  https://sfl.gl/ZVPq
+  Replace Elite :
+  https://sfl.gl/7iTbDytT
+  Replace Special :
+  https://sfl.gl/yMuAf
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/as4jlvEpC1w",
     categories: ["Sanrio"]
   },
   {
     title: "SHARE SCRIPT SKIN CLAUDE SANRIO - BAD BRO",
     description: "⚙ Fitur :\nFull Effect, Full Audio, Support All Graphic, No Delay, Hd Skin, No Banned, No Root, Support All Mode.",
-    content: "🗂️ Download Script :\nReplace Default :\nhttps://sfl.gl/DfQsPY\nReplace Basic :\nhttps://sfl.gl/0CmRDr5\nReplace Summer :\nhttps://sfl.gl/KzWZ\nReplace Starlight :\nhttps://sfl.gl/3xgjlG5\n\n⚠️ Cara Memasang\nLink\nUpdate Artikel 27/9/25",
+    content: `🗂️ Download Script :
+  Replace Default :
+  https://sfl.gl/DfQsPY
+  Replace Basic :
+  https://sfl.gl/0CmRDr5
+  Replace Summer :
+  https://sfl.gl/KzWZ
+  Replace Starlight :
+  https://sfl.gl/3xgjlG5
+
+  ⚠️ Cara Memasang
+  Link
+  Update Artikel 27/9/25`,
     youtubeUrl: "https://youtu.be/eUiyXPr2L-o",
     categories: ["Sanrio"]
   }
